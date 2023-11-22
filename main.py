2d49c03df387230ae6ca58d14f0413a8905685f2
Timur Nagimov <nagimov_timur@bk.ru>
2023-11-22 08:53:08 +0300

    added type hints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику '
                 f'равный {5 + randint(3, 5)}')
@@ -13,7 +13,7 @@ def attack(char_name, char_class):
                 f'равный {5 + randint(-3, -1)}')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -22,7 +22,7 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -32,7 +32,7 @@ def special(char_name, char_class):
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -44,7 +44,7 @@ def start_training(char_name, char_class):
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = ''
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -56,9 +56,9 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого '
                            'хочешь играть: Воитель — warrior, '
@@ -78,16 +78,51 @@ def choice_char_class():
     return char_class
 
 
-def main():
+# Тестовые данные.
+TEST_DATA: list[tuple[int, str, bool]] = [
+    (44, 'success', True),
+    (16, 'failure', True),
+    (4, 'success', False),
+    (21, 'failure', False),
+]
+
+BONUS: float = 1.1
+ANTIBONUS: float = 0.8
+
+
+def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
+    current_rep += rep_points
+    if buf_effect:
+        return current_rep * BONUS
+    return current_rep
+
+
+def remove_rep(current_rep: float, rep_points: int,
+               debuf_effect: bool) -> float:
+    current_rep -= rep_points
+    if debuf_effect:
+        return current_rep * ANTIBONUS
+    return current_rep
+
+
+def main(duel_res: list[tuple[int, str, bool]]) -> str:
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
+    current_rep: float = 0.0
+    for rep, result, effect in duel_res:
+        if result == 'success':
+            current_rep = add_rep(current_rep, rep, effect)
+        if result == 'failure':
+            current_rep = remove_rep(current_rep, rep, effect)
+    return (
+        f'После {len(duel_res)} поединков, '
+        f'репутация персонажа — {current_rep:.3f} очков.'
+    )
